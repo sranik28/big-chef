a5234f463e7ff33c0a37cf798652bf8722537a57
sranik28~ <anikshariarrahman@gmail.com>
2023-05-02 02:13:21 +0600

    first question added

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -5,6 +5,7 @@ const Blog = () => {
     return (
         <main className='container'>
             <NavBar />
+            <h1 className='text-3xl font-bold text-center my-5'>Question's <span className='text-yellow-500'>Answer....</span></h1>
             <div className='grid md:grid-cols-2 gap-4 mt-10 mb-20'>
                 <div className='p-3 border-2 rounded w-full h-[100%]'>
                     <h1 className='text-3xl py-3'>Tell us the differences between uncontrolled and controlled components?</h1>
