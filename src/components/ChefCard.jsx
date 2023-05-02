4de7cf293d93109aced2a031c42ddd2e654c2604
sranik28~ <anikshariarrahman@gmail.com>
2023-05-03 02:21:29 +0600

    see more btn added

--- src/components/ChefCard.jsx
+++ src/components/ChefCard.jsx
@@ -3,8 +3,10 @@ import { Link } from 'react-router-dom';
 import { BiLike } from 'react-icons/bi';
 
 const ChefCard = ({ chef }) => {
-    const { name, experience, number_of_recipes, likes, image } = chef
-    console.log()
+    const { name, experience, number_of_recipes, likes, image } = chef;
+
+    
+
     return (
         <main className="max-w-[800px] mx-auto mt-20 grid md:grid-cols-2 mb-10 border rounded border-orange-300 p-6">
             <div>
@@ -12,11 +14,12 @@ const ChefCard = ({ chef }) => {
             </div>
             <div className=''>
                 <h1 className="text-4xl font-bold py-5">{name}</h1>
-                <p className="text-2xl my-2">The Best Number Of Recipes : {number_of_recipes}</p>
-                <p className="text-2xl my-2"> Experience : {experience}</p>
-                <p className="text-2xl flex items-center gap-2 my-2"><span className='text-yellow-600'><BiLike /></span> {likes}</p>
-                <Link to="/view-details"><button className="bg-yellow-600 w-full py-2 mt-10 rounded-md text-lg font-semibold text-white">View Details</button></Link>
+                <p className="text-2xl my-2 text-gray-500">The Best Number Of Recipes : {number_of_recipes}</p>
+                <p className="text-2xl my-2 text-gray-500"> Experience : {experience}</p>
+                <p className="text-2xl flex items-center gap-2 my-2 text-gray-500"><span className='text-yellow-600 '><BiLike /></span> {likes}</p>
+                <Link to="/view-details"><button className="bg-yellow-600 hover:bg-amber-500 w-full py-2 mt-10 rounded-md text-lg font-semibold text-white">View Details</button></Link>
             </div>
+            
         </main>
     );
 };
--- src/components/Chefs.jsx
+++ src/components/Chefs.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useApiData } from '../context/ApiDataProvider';
 import ChefCard from './ChefCard';
 
 const Chefs = () => {
 
     const { chefs } = useApiData()
+
+    const [cardCount, setCardCount] = useState(3)
+    const [seeMore, setSeeMore] = useState(true);
+
+    const slider = chefs.slice(0, cardCount);
+
+
+    const handelSeeMoreBtn = () => {
+        setCardCount(chefs.length);
+        setSeeMore(false)
+    }
+
     return (
         <div>
             {
-                chefs.map(chef => <ChefCard key={chef._id} chef={chef} />)
+                slider.map(chef => <ChefCard key={chef._id} chef={chef} />)
             }
+            <button onClick={handelSeeMoreBtn} className={`bg-yellow-600 py-2 px-5 block mx-auto rounded-md hover:hover:bg-amber-500 text-white font-semibold ${seeMore ? "" : "hidden"}`}>See More</button>
         </div>
     );
 };
