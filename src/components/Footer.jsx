daa08b144c837cbe75958a200a663865a79cc4ce
sranik28~ <anikshariarrahman@gmail.com>
2023-05-04 02:48:02 +0600

    footer UI update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,34 +1,186 @@
-import React from 'react';
-import { FaFacebookF, FaLinkedinIn, FaTwitter, FaInstagram } from 'react-icons/fa';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
-        <main className='bg-slate-900 mt-5'>
-            <div className='grid md:grid-cols-3 gap-5 max-w-[1240px] mx-auto py-5 px-6 text-white'>
-                <div className=' text-center'>
-                    <h2 className='text-xl my-8'>Contact Us</h2>
-                    <p className='mb-2'>Location : Mohakhali,Dhaka-1215</p>
-                    <p className='mb-2'>Call : 017-4182-0638</p>
-                    <p>Email : bigchef@gmail.com</p>
-                </div>
-                <div className='text-center'>
-                    <h2 className='text-xl my-8'>Big Chef</h2>
-                    <p>Necessary, making this the first true generator on <br /> the Internet. It uses a dictionary of over 200</p>
-                    <div className=' flex gap-2 justify-center mt-3'>
-                        <FaFacebookF />
-                        <FaInstagram />
-                        <FaTwitter />
-                        <FaLinkedinIn />
-                    </div>
+        <footer className="bg-zinc-300 mt-10 rounded-md">
+            <div className="max-w-6xl mx-auto py-8 px-4 sm:px-6 lg:py-16 lg:px-8">
+                <div className="xl:grid xl:grid-cols-3 xl:gap-8">
+                    <div className="grid grid-cols-2 gap-8 xl:col-span-2">
+                        <div className="md:grid md:grid-cols-2 md:gap-8">
+                            <div>
+                                <h4 className="text-sm leading-5 font-semibold tracking-wider text-gray-400 uppercase">
+                                    About
+                                </h4>
+                                <ul className="mt-4">
+                                    <li>
+                                        <Link
+                                            to="/"
+                                            className="text-base leading-6 text-black hover:text-amber-500"
+                                        >
+                                            Home
+                                        </Link>
+                                    </li>
+                                    <li className="mt-4">
+                                        <Link
+                                            to="/blog"
+                                            className="text-base leading-6 text-black hover:text-amber-500"
+                                        >
+                                            Blog
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="mt-12 md:mt-0">
+                                <h4 className="text-sm leading-5 font-semibold tracking-wider text-gray-400 uppercase">
+                                    Recipes
+                                </h4>
+                                <ul className="mt-4">
+                                    <li>
+                                        <Link
+                                            to="#"
+                                            className="text-base leading-6 text-black hover:text-amber-500"
+                                        >
+                                            Main Dishes
+                                        </Link>
+                                    </li>
+                                    <li className="mt-4">
+                                        <Link
+                                            to="#"
+                                            className="text-base leading-6 text-black hover:text-amber-500"
+                                        >
+                                            Desserts
+                                        </Link>
+                                    </li>
+                                    <li className="mt-4">
+                                        <Link
+                                            to="#"
+                                            className="text-base leading-6 text-black hover:text-amber-500"
+                                        >
+                                            Drinks
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="md:grid md:grid-cols-2 md:gap-8">
+                            <div>
+                                <h4 className="text-sm leading-5 font-semibold tracking-wider text-gray-400 uppercase">
+                                    Follow Us
+                                </h4>
+                                <ul className="mt-4">
+                                    <li>
+                                        <Link
+                                            to="#"
+                                            className="text-base leading-6 text-black hover:text-amber-500"
+                                        >
+                                            Facebook
+                                        </Link>
+                                    </li>
+                                    <li className="mt-4">
+                                        <Link
+                                            to="#"
+                                            className="text-base leading-6 text-black hover:text-amber-500"
+                                        >
+                                            Instagram
+                                        </Link>
+                                    </li>
+                                    <li className="mt-4">
+                                        <Link
+                                            to="#"
+                                            className="text-base leading-6 text-black hover:text-amber-500"
+                                        >
+                                            Twitter
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="mt-12 md:mt-0">
+                                <h4 className="text-sm leading-5 font-semibold tracking-wider text-gray-400 uppercase">
+                                    Subscribe
+                                </h4>
+                                <p className="mt-4 text-base leading-6 text-black">
+                                    Get the latest recipes delivered straight to your inbox.
+                                </p>
 
+                                <form className="mt-4 sm:flex">
+                                    <label htmlFor="email" className="sr-only">
+                                        Email
+                                    </label>
+                                    <input
+                                        id="email"
+                                        type="email"
+                                        placeholder="Enter your email"
+                                        className="appearance-none w-full bg-gray-700 border border-gray-600 rounded-md py-2 px-4 text-black leading-tight focus:outline-none focus:bg-gray-600 focus:border-gray-700"
+                                    />
+                                    <button
+                                        type="submit"
+                                        className="mt-3 sm:mt-0 sm:ml-3 bg-gray-600 hover:bg-gray-500 text-white py-2 px-4 rounded-md transition-colors duration-300"
+                                    >
+                                        Subscribe
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mt-12 md:mt-16 xl:mt-0">
+                        <h4 className="text-sm leading-5 font-semibold tracking-wider text-gray-400 uppercase">
+                            Popular Recipes
+                        </h4>
+                        <div className="mt-4">
+                            <Link
+                                to="#"
+                                className="text-base leading-6 text-black hover:text-amber-500"
+                            >
+                                coconut milk
+                            </Link>
+                            <div className="mt-2">
+                                <Link
+                                    to="#"
+                                    className="text-base leading-6 text-black hover:text-amber-500"
+                                >
+                                    Jerk chicken with rice & peas
+                                </Link>
+                            </div>
+                            <div className="mt-2">
+                                <Link
+                                    to="#"
+                                    className="text-base leading-6 text-black hover:text-amber-500"
+                                >
+                                    glass noodles
+
+                                </Link>
+                            </div>
+                            <div className="mt-2">
+                                <Link
+                                    to="#"
+                                    className="text-base leading-6 text-black hover:text-amber-500"
+                                >
+                                    Tom Yum Goong
+                                </Link>
+                                <Link
+                                    to="#"
+                                    className="text-base leading-6 text-black hover:text-amber-500"
+                                >
+                                    mini wrap
+                                </Link>
+                                <Link
+                                    to="#"
+                                    className="text-base leading-6 text-black hover:text-amber-500"
+                                >
+                                    traditional Thai fish recipe
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className='text-center'>
-                    <h1 className='text-xl my-8'>Opening Hours</h1>
-                    <p className='mb-2'>Everyday</p>
-                    <p>10.00 Am -10.00 Pm</p>
+                <div className="mt-12 border-t border-gray-700 pt-8">
+                    <p className="text-base leading-6 text-gray-400 xl:text-center">
+                        © 2023 Recipe Website. All rights reserved.
+                    </p>
                 </div>
             </div>
-        </main>
+        </footer>
     );
 };
 
